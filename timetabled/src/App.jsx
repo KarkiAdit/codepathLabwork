4460cf42ff49f5f76cd3ee013538a06db7e6d5b2
KarkiAdi <adityakarki728@gmail.com>
2023-02-26 10:35:24 -0600

    Completed lab work 1

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calender from './components/Calender'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 days in Chicago</h1>
+      <h2>Welcome to Chicago. Aditya! Check out this calender to get to know the city and see all the sights during your stay.</h2>
+      <Calender/>
+    </div>
+  )
+}
+
+export default App
+
